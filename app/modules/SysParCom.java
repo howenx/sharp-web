22c2b87c8c174057e5a5ac0818e1ce3972ee159a
tiffany <tiffany@tiffanydeiMac.local>
2016-03-11 17:24:45 +0800

    index page

--- app/modules/SysParCom.java
+++ app/modules/SysParCom.java
@@ -15,10 +15,16 @@ public class SysParCom {
     public static String INDEX_PAGE;
     public static final OkHttpClient client = new OkHttpClient();
     public static String LOGIN_PAGE;
+    public static String ITEM_PAGE;
+    public static String IHEME_PAGE;
+    public static String PIN_PAGE;
     @Inject
     public SysParCom(Configuration configuration) {
 
         INDEX_PAGE = configuration.getString("products.page.index");
         LOGIN_PAGE = configuration.getString("user.page.login");
+        ITEM_PAGE = configuration.getString("products.page.item");
+        IHEME_PAGE = configuration.getString("products.page.theme");
+        PIN_PAGE = configuration.getString("products.page.pin");
     }
 }
