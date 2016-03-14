1a3a0acb9f276fc3f07b3d402c5b87cae6e1f95b
tiffany <tiffany@tiffanydeiMac.local>
2016-03-14 13:21:14 +0800

    index page

--- app/controllers/ProductsCtrl.java
+++ app/controllers/ProductsCtrl.java
@@ -6,6 +6,7 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import domain.Slider;
 import domain.Theme;
+import play.Logger;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -20,10 +21,10 @@ import static modules.SysParCom.*;
 public class ProductsCtrl extends Controller {
     //首页
     public Result index() throws Exception {
-        List<Slider> sliderList = new ArrayList<Slider>();
-        List<Theme> themeList = new ArrayList<Theme>();
+        List<Slider> sliderList = new ArrayList<>();
+        List<Theme> themeList = new ArrayList<>();
         Request request = new Request.Builder()
-                .url(INDEX_PAGE)
+                .url(INDEX_PAGE + "1")
                 .build();
         Response response = client.newCall(request).execute();
         if(response.isSuccessful()){
@@ -76,6 +77,42 @@ public class ProductsCtrl extends Controller {
         }
         return ok(views.html.products.index.render(sliderList,themeList));
     }
+
+    //Ajax加载首页
+    public Result loadIndexAjax() throws Exception{
+        String pageCount = request().body().asJson().toString();
+        Logger.error("********************");
+        Logger.error(INDEX_PAGE + pageCount);
+        Logger.error("********************");
+        List<Theme> themeList = new ArrayList<>();
+        Request request = new Request.Builder()
+                .url(INDEX_PAGE + pageCount)
+                .build();
+        Response response = client.newCall(request).execute();
+        if(response.isSuccessful()){
+            JsonNode json = Json.parse(response.body().string());
+            if(json.has("theme")){
+                JsonNode themeJson = json.get("theme");
+                for(JsonNode themeTemp : themeJson){
+                    Theme theme = Json.fromJson(themeTemp,Theme.class);
+                    JsonNode imgJson = Json.parse(theme.getThemeImg());
+                    String imgUrl = imgJson.get("url").toString();
+                    imgUrl = imgUrl.substring(1,imgUrl.length()-1);
+                    theme.setThemeImg(imgUrl);
+                    if(!"h5".equals(theme.getType())){
+                        String themeUrl = theme.getThemeUrl();
+                        //themeUrl = themeUrl.replace("http://172.28.3.51:9001/topic/list/","");
+                        themeUrl = themeUrl.replace(IHEME_PAGE,"");
+                        theme.setThemeUrl(themeUrl);
+                    }
+                    themeList.add(theme);
+                }
+            }
+        }
+        return ok(Json.toJson(themeList));
+
+    }
+
 
 
     //商品明细
