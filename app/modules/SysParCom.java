a8c533af97c528095f8848e5178bea346518bb18
tiffany <tiffany@tiffanydeiMac.local>
2016-03-15 15:27:36 +0800

    goods detail

--- app/modules/SysParCom.java
+++ app/modules/SysParCom.java
@@ -30,9 +30,10 @@ public class SysParCom {
     public static String REGIST_CODE;//用户注册发送短信验证码
     public static String REGIST_PAGE;//用户注册
 
-    public static String ITEM_PAGE;
-    public static String THEME_PAGE;
-    public static String PIN_PAGE;
+    public static String ITEM_PAGE;//普通商品,多样化价格商品
+    public static String THEME_PAGE;//主题
+    public static String PIN_PAGE;//拼购商品
+    public static String SUBJECT_PAGE;//主题中自定义价格商品
 
 
     public static String IMAGE_CODE;//图形验证码
@@ -79,6 +80,7 @@ public class SysParCom {
         ITEM_PAGE = configuration.getString("products.page.item");
         THEME_PAGE = configuration.getString("products.page.theme");
         PIN_PAGE = configuration.getString("products.page.pin");
+        SUBJECT_PAGE = configuration.getString("products.page.subject");
 
         IMAGE_CODE =configuration.getString("user.page.imagecode");
 
