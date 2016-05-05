f492434f10f5a68abea9d4eb2b04338218e74893
sibyl <sibyl.sun@kakaocorp.com>
2016-05-05 17:47:40 +0800

    jedis暂时注释

--- app/actor/MnsActor.java
+++ app/actor/MnsActor.java
@@ -24,7 +24,7 @@ public class MnsActor extends AbstractActor {
             if (event instanceof ILoggingEvent) {
                 ((ILoggingEvent) event).getMDCPropertyMap().put("projectId", "style-web");
 //                System.out.println("日志Json格式---->" + Json.toJson(event).toString());
-                jedis.publish(REDIS_CHANNEL, Json.toJson(event).toString());
+//                jedis.publish(REDIS_CHANNEL, Json.toJson(event).toString());
             }
         }).matchAny(s -> {
             Logger.error("MnsActor received messages not matched: {}", s.toString());
