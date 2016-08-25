a5394d91d68368e7f46deccb7bfc61c2e38c59f0
sibyl <sibyl.sun@kakaocorp.com>
2016-08-25 14:08:33 +0800

    http

--- app/filters/ProdFilters.scala
+++ app/filters/ProdFilters.scala
@@ -8,8 +8,9 @@ import play.api.http.HttpFilters
   * Created by howen on 15/12/28.
   */
 class ProdFilters @Inject()(log: LoggingFilter,
-                            tls:TLSFilter,
+//                            tls:TLSFilter,
                             gzip: play.filters.gzip.GzipFilter
                        ) extends HttpFilters {
-    var filters = Seq(log, tls,gzip);
+//    var filters = Seq(log, tls,gzip);
+      var filters = Seq(log, gzip);
 }
