0d14b2ce71b31f0aabc6eaa5ca6a2de24333ca11
celine <celine.zhang@kakaocorp.com>
2016-08-29 15:25:42 +0800

    ab

--- public/javascripts/hp.js
+++ public/javascripts/hp.js
@@ -39,10 +39,9 @@ $(function() {
 	    echo.init();
 	}
 
-	$('.foot .buy').click(function(){
-		$('.icon02').css('background','url(/assets/images/02.png)');
-	});
-
+	('.foot .buy').click(function(){
+		$('.icon02').css({'background':'url(images/02.png)'});
+	})
 
 });
 
